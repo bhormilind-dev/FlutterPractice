44851d5eef267f9ce6ca48038a775df56783b14d
Milind Bhor <94182634+bhormilind-dev@users.noreply.github.com>
2022-02-06 08:30:02 -0500

    Fond - Adding Images

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Center(
-        child: Text('Simple App'),
+void main() => runApp(MTest());
+
+class MTest extends StatelessWidget {
+  const MTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Simple App',
+              style: TextStyle(color: Colors.amber),
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900]),
+      body: Stack(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/background.jpg'),
+                      fit: BoxFit.cover))),
+          Center(
+              child: Text('Hello, Welcome!',
+                  style: TextStyle(
+                      color: Colors.grey[350],
+                      fontFamily: 'Dacing',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 32.0,
+                      decoration: TextDecoration.underline)))
+        ],
       ),
-      backgroundColor: Colors.teal[600],
-
-    ),
-    body: Center(
-      child: Text('Hello, Welcome!',
-      style: TextStyle(
-       fontFamily: 'Dacing',
-       fontWeight: FontWeight.w700,
-       fontSize: 32.0
-      )),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click',
-      style: TextStyle(
-        fontFamily: 'Dacing',
-          fontWeight: FontWeight.w700
-      )),
-      onPressed: () => {},
-      backgroundColor: Colors.teal[600],
-    ),
-  )
-));
-
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click',
+            style: TextStyle(
+                color: Colors.amber,
+                fontFamily: 'Dacing',
+                fontWeight: FontWeight.w700)),
+        onPressed: () => {},
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ));
+  }
+}
