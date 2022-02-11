193d2bde8870d51fbb6477df094df886544f5b23
Milind Bhor <94182634+bhormilind-dev@users.noreply.github.com>
2022-02-10 21:59:41 -0500

    Displaying 'Quotes' with Material Card

--- QuoteApp/lib/main.dart
+++ QuoteApp/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quote_app/_common/_models/Quotes.dart';
+import 'package:quote_app/_common/_models/Quote.dart';
 
 
 void main() {
@@ -12,9 +12,43 @@ class MyApp extends StatelessWidget {
 
   List<Quotes> quotes = [
     Quotes(text: 'Never Give Up', author: 'Unknown'),
-    Quotes(text: 'Fight hard', author: 'Unknown Author'),
+    Quotes(text: 'Fight hard adasdasdas dasasd asd asd asd', author: 'Unknown Author'),
     Quotes(text: 'Be Smart', author: 'Unknown'),
   ];
+
+  Widget getQuoteWidget(Quotes quote) {
+    return Card(
+      color: Colors.grey[850],
+      margin: const EdgeInsets.fromLTRB(16.0, 16.8, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0,0,10,10),
+        child: Column(
+          children: [
+              ListTile(
+                leading: const Icon(Icons.wb_sunny_outlined, color: Colors.greenAccent),
+                title: Text(quote.text,
+                style: const TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white
+                )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(' - ${quote.author}',
+                    style: const TextStyle(
+                        fontSize: 11.0,
+                        color: Colors.amber,
+                        fontStyle: FontStyle.italic
+                    )),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,17 +61,11 @@ class MyApp extends StatelessWidget {
         foregroundColor: Colors.white,
         elevation: 0.0,
       ),
-      body:  Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: quotes.map((e) => Text('${e.text} - ${e.author}', style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0
-          ),)).toList()
+          children: quotes.map((e) => getQuoteWidget(e)).toList()
         ),
-      ),
     );
   }
 }
